bff6111e81fe75a5e4f92b4f8fe6777c749a220d
hikmatyar22 <abiq5161@gmail.com>
2024-06-13 11:39:42 +0700

    membuat void in order dan beberapa kondisi ifnya

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -66,6 +66,21 @@ public:
 				currentNode = currentNode->rightchild;
 		}
 	} 
+
+	void inorder(Node* ptr)
+	{
+		if (ROOT == nullptr)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != nullptr)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
 };
 
 int main() {
