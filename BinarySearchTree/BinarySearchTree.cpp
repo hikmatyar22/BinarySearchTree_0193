464710438c80fd6845909f59f77442e9fee3999f
hikmatyar22 <abiq5161@gmail.com>
2024-06-13 11:36:53 +0700

    membuat prosedur insert dan membuat bebrapa program node didalamnya

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -25,6 +25,20 @@ public:
 	BinaryTree() {
 		ROOT = nullptr; //intializing ROOT to null
 	}
+
+	void insert(string element)// insert a node in the binary search tree 
+	{
+		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the new node
+		newNode->info = element; // Assign value to the data field of the new node
+		newNode->leftchild = nullptr; // Make the left child of the nwq node point to null
+		newNode->rightchild = nullptr; // Make the right child of the nwq node point to null
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+		search(element, parent, currentNode); // locate the node which will be the parent of the node to be ins
+
+		
+	}
 };
 
 int main() {
